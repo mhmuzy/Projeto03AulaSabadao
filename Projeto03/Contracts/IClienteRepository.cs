95cce341e44369ee6623f9c00d70f12354086d04
marcio henrique <mhmuzy@embratel.com.br>
2020-04-01 20:11:35 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto03/Contracts/IClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//localização ()/
+namespace Projeto03.Contracts
+{
+    interface IClienteRepository
+    {
+
+    }
+}
---
+++ Projeto03/Entities/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto03.Entities
+{
+    public class Cliente
+    {
+        //prop + 2xtab
+        public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
