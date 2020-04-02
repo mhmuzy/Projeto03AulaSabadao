4f171b3d33dc76973be6139f5459cae706b14856
marcio henrique <mhmuzy@embratel.com.br>
2020-04-01 22:22:56 -0300

    Treinamento

--- Projeto03/Program.cs
+++ Projeto03/Program.cs
@@ -12,22 +12,53 @@ namespace Projeto03
             {
                 Console.WriteLine("\n - SISTEMA DE CONTROLE DE CLIENTES - \n");
 
+                Console.WriteLine();
+
+                Console.WriteLine("Cadastrar cliente (1)");
+                Console.WriteLine("Consultar cliente (2)");
+                Console.WriteLine();
+                Console.WriteLine("Digite a opção desejada:");
+                int num = int.Parse(Console.ReadLine());
                 var cliente = new Cliente(); //objeto
-
-                Console.Write("Informe o nome do cliente..........: ");
-                cliente.Nome = Console.ReadLine();
-
-                Console.Write("Informe o email do cliente.........: ");
-                cliente.Email = Console.ReadLine();
-
-                cliente.DataCriacao = DateTime.Now; //data do sistema!
-
-                //instanciando o repositorio..
+                                                                         //instanciando o repositorio..
                 var repository = new ClienteRepository();
-                repository.Inserir(cliente);
 
-                Console.WriteLine("\nCliente cadastrado com sucesso.");
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine();
+                        Console.Write("Informe o nome do cliente..........: ");
+                        cliente.Nome = Console.ReadLine();
 
+                        Console.Write("Informe o email do cliente.........: ");
+                        cliente.Email = Console.ReadLine();
+
+                        cliente.DataCriacao = DateTime.Now; //data do sistema!
+
+                        repository.Inserir(cliente);
+
+                        Console.WriteLine("\nCliente cadastrado com sucesso.");
+                        break;
+                    case 2:
+                        //exibindo os clientes cadastrados na base de dados
+                        var lista = repository.Consultar();
+
+                        Console.WriteLine();
+                        Console.WriteLine("Consulta de Clientes");
+
+                        foreach (var item in lista)
+                        {
+                            //item -> representa cada cliente contido na lista
+                            Console.WriteLine("Id do(a) cliente: " + item.IdCliente + ", Nome do(a) cliente: " + item.Nome + ", E - mail do cliente: " + item.Email + ", Data de Criação: " + item.DataCriacao.ToString("dd/MM/yyyy"));
+
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                
+                
             }
             catch (Exception e)
             {
