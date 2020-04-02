b21a84d6e673c7f4a93eb5f1ac945716f28e4dcf
marcio henrique <mhmuzy@embratel.com.br>
2020-04-01 21:05:19 -0300

    Treinamento

--- Projeto03/Contracts/IClienteRepository.cs
+++ Projeto03/Contracts/IClienteRepository.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Projeto03.Entities; //importando
 
-//localização ()/
+//localização (namespace)/
 namespace Projeto03.Contracts
 {
-    interface IClienteRepository
+    //declaração da interface
+    public interface IClienteRepository
     {
-
+        //método abstrato
+        void Inserir(Cliente cliente);
+        void Atualizar(Cliente cliente);
+        void Excluir(Cliente cliente);
+        List<Cliente> Consultar();
+        Cliente ObterPorId(int id);
     }
 }
---
+++ Projeto03/Repositories/ClienteRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Projeto03.Entities; //importando
+using Projeto03.Contracts; //importando
+using System.Data.SqlClient; //importando
+using Dapper; //importando
+
+namespace Projeto03.Repositories
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        //declarar a connectionstring como um atributo (campo)
+        //private -> acesso permitido somente dentro da propria classe
+        private string connectionString;
+
+        //método construtor (responsavel por inicializar os atributos da classe)
+        //ctor + 2x[tab]
+        public ClienteRepository()
+        {
+
+        }
+
+        public void Inserir(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> Consultar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
